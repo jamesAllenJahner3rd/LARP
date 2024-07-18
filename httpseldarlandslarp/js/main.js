14d37e91d6d7cc72ced3f83ba3cb7072363c8f16
KindWeb Creations <jitone1@gmail.com>
2024-07-18 03:01:48 -0500

    Revert "Upcoming events."
    This reverts commit 03aa7ca6313d9884b8b0e6926a3ff3ef106ce5bb.

--- httpseldarlandslarp/js/main.js
+++ httpseldarlandslarp/js/main.js
@@ -10,11 +10,6 @@ Array.from(imgStraighten).forEach((element,index) =>   element.addEventListener(
         stickyNoteSlide[index].style.opacity = '1';
         messText[index].style.opacity='1';
     }));
-let tableAppear = document.querySelector(".practiceDates");
-let scrollAppear =document.querySelector(".scroll");
-tableAppear.addEventListener("click", () => { scrollAppear.removeAttribute("hidden");
-
-});
 
 
 
