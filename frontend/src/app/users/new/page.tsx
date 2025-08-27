b7257addebdf31366bc5902a7a7b2c7f566c132b
James Jahner <jitone1@gmail.com>
2025-08-27 13:51:39 -0500

    Added DaisyUI

---
+++ frontend/src/app/users/new/page.tsx
@@ -0,0 +1,11 @@
+//page.tsx the routing Should be lowercase based on convention not configuration Delete
+//If I add any other files to this folder users page will not be accessible
+import React from 'react'
+
+const NewUserPage = () => {
+  return (
+    <div>NewUserPage</div>
+  )
+}
+
+export default NewUserPage
