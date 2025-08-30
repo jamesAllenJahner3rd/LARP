d92b161d630801a1fc102c601bb96c8c47d4b2de
James Jahner <jitone1@gmail.com>
2025-08-30 09:31:28 -0500

    add authorize to ...nextauth; npm i bcryptjs,  npm i -D @types/bcryptjs

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -3,11 +3,41 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/prisma/client"; // Adjust the import path as necessary
 import { adapter } from "next/dist/server/web/adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { email } from "zod";
+import bcrypt from "bcryptjs";
 
 // This had to be removed from the handler and exported to the main page so that we can get session data to the server
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "you@example.com" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your Password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user) return null;
+        // Here you would validate the password
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.passwordHash!
+        );
+        if (!passwordMatch) return null;
+        else return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
