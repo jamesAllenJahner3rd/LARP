4a8562963eb64c5954ec1d457a070c473f67ddab
James Jahner <jitone1@gmail.com>
2025-09-02 21:19:26 -0500

    3. Created a dynamic Navbar for mobile first

---
+++ src/app/components/HamburgerMenu.tsx
@@ -0,0 +1,104 @@
+
+type Props = {
+    openMenu: boolean;
+    setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const HamburgerMenu = ({ openMenu, setOpenMenu }: Props) => {
+
+
+
+    const spanCss = "transition-all  duration-1000 ease-in-out block absolute h-[9px] w-full bg-black rounded-[9px]"
+    const span1 = openMenu ? "top-[18px] rotate-135" : "top-0 rotate-0";
+    const span2 = openMenu ? " opacity-0 left-[-60px]" : "opacity-100 left-0";
+    const span3 = openMenu ? "top-[18px] -rotate-135" : "top-[36px] rotate-0"
+    return (
+        <div id="nav-icon1" className={`transition delay-150 duration-50 ease-in-out w-[60px] h-[45px] my-[50px] mx-auto rotate-0 cursor-pointer fixed right-10  md:hidden lg:hidden `} onClick={() => setOpenMenu(toggle => !toggle)}>
+            <span className={`${spanCss} opacity-100 left-0 ${span1}`} ></span>
+            <span className={`${spanCss} top-[18px] ${span2}`}></span>
+            <span className={`${spanCss} opacity-100 left-0  ${span3}`}></span>
+        </div >
+    )
+}
+
+export default HamburgerMenu
+/* Icon 1 */
+/** {bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ...
+ * transition-property: transform, translate, scale, rotate;
+transition-timing-function: var(--default-transition-timing-function);
+ cubic-bezier(0.4, 0, 0.2, 1) 
+transition-duration: var(--default-transition-duration); /* 150ms 
+    margin: 0;
+    padding: 0;
+}
+#nav - icon1{
+    width: 60px;
+    height: 45px;
+    position: relative;
+    margin: 50px auto;
+    -webkit - transform: rotate(0deg);
+    -moz - transform: rotate(0deg);
+    -o - transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit - transition: .5s ease -in -out;
+    -moz - transition: .5s ease -in -out;
+    -o - transition: .5s ease -in -out;
+    transition: .5s ease -in -out;
+    cursor: pointer;
+}
+
+#nav - icon1 span, {
+    display: block;
+    position: absolute;
+    height: 9px;
+    width: 100 %;
+    background: #d3531a;
+    border - radius: 9px;
+    opacity: 1;
+    left: 0;
+    -webkit - transform: rotate(0deg);
+    -moz - transform: rotate(0deg);
+    -o - transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit - transition: .25s ease -in -out;
+    -moz - transition: .25s ease -in -out;
+    -o - transition: .25s ease -in -out;
+    transition: .25s ease -in -out;
+}
+
+#nav - icon1 span: nth - child(1) {
+    top: 0px;
+}
+
+#nav - icon1 span: nth - child(2) {
+    top: 18px;
+}
+
+#nav - icon1 span: nth - child(3) {
+    top: 36px;
+}
+
+#nav - icon1.open span: nth - child(1) {
+    top: 18px;
+    -webkit - transform: rotate(135deg);
+    -moz - transform: rotate(135deg);
+    -o - transform: rotate(135deg);
+    transform: rotate(135deg);
+}
+
+#nav - icon1.open span: nth - child(2) {
+    opacity: 0;
+    left: -60px;
+}
+
+#nav - icon1.open span: nth - child(3) {
+    top: 18px;
+    -webkit - transform: rotate(-135deg);
+    -moz - transform: rotate(-135deg);
+    -o - transform: rotate(-135deg);
+    transform: rotate(-135deg);
+}
+$(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
+});*/
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,21 +1,39 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import HamburgerMenu from './HamburgerMenu'
+
+
+
 
 
 const NavBar = () => {
-    const navCss = "m-auto min-w-fit grid-cols-4 max-w-fit grid py-2 px-3 items-center justify-items-center w-content border-1 rounded-full fixed inset-x-0 mx-auto bottom-6"
+    const [openMenu, setOpenMenu] = useState(false);
+    const navCss = `grid grid-rows-4 min-h-5 min-w-fit grid py-8 px-3 items-center justify-items-center w-content border-1 rounded-full fixed inset-y-0 max-h-fit m-auto max-w-fit transition-all duration-1000 ease-in-out ${openMenu ? "inset-x-0 opacity-100" : "inset-x-100 opacity-0"}`
+    const navMedium = "md:m-auto md:min-w-fit md:grid-cols-4 md:max-w-fit md:grid md:py-2 md:px-3 md:items-center md:justify-items-center md:w-content md:border-1 md:rounded-full md:fixed md:inset-x-0 md:mx-auto md:bottom-6 md:grid-rows-1 md:opacity-100 md:inset-y-300"
 
     return (
-        <><nav className={navCss}>
-            <Link href="" className="btn-ghost"><h3 className=''>Welcome </h3></Link>
-            <Link href="" className=""><h3 className=''>Explore</h3></Link>
-            <Link href="" className=""><h3 className=''>Members</h3></Link>
-            <Link href="" className=""><h3 className=''>Shop</h3></Link>
+        <>
+            <HamburgerMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
+            <nav className={`${navCss}  ${navMedium}`}>
+                <Link href="" className=""><h3 className=''>Welcome </h3></Link>
+                <Link href="" className=""><h3 className=''>Explore</h3></Link>
+                <Link href="" className=""><h3 className=''>Members</h3></Link>
+                <Link href="" className=""><h3 className=''>Shop</h3></Link>
 
-        </nav >
-            <button className="btn btn-ghost" type="button">hi</button>
+            </nav >
+
+
+
         </>
     )
 }
 
 export default NavBar
+{/* <div id="nav-icon1">
+  <span></span>
+  <span></span>
+  <span></span>
+</div> */}
+
+
