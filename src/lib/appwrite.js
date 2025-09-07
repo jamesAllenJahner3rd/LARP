b8649f7215634eebfff3cd0a9e83349a3137ff44
James Jahner <jitone1@gmail.com>
2025-09-07 01:31:24 -0500

    1.2 guarding the endpoints

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,8 +1,13 @@
 import { Client, Account, Databases } from "appwrite";
 
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+const client = new Client();
+if (typeof window !== "undefined") {
+  client.setEndpoint("https://nyc.cloud.appwrite.io/v1");
+}
+if (process.env.NODE_ENV !== "production") {
+  client.setEndpoint("https://nyc.cloud.appwrite.io/v1");
+}
+client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
 const databases = new Databases(client);
