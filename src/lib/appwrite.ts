ecd0c09f8361b8ad49d89a2a96e178ff52a3914f
James Jahner <jitone1@gmail.com>
2025-10-22 22:27:35 -0500

    Changed the end point for the client adding nyc

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,7 +4,8 @@ const client = new Client();
 
 client
   .setEndpoint(
-    process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1",
+    process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT ||
+      "https://nyc.cloud.appwrite.io/v1",
   )
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 export const account = new Account(client);
