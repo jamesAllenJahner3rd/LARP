cb5b460f5675697c434bb617ff950c2e76c57b93
James Jahner <jitone1@gmail.com>
2026-01-15 01:34:35 -0600

    fix

--- src/app/api/slack/stream/route.ts
+++ src/app/api/slack/stream/route.ts
@@ -1,8 +1,5 @@
 
 //appwrite is not enabled streaming on Appwrite hosting
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
-import { addClient, removeClient } from "@/lib/sse";
 /**
  * GET /api/slack/stream
  *
